28d559049af58d9fa981c04f128b1e6cb7571394
wenject <2256475223@qq.com>
2026-02-12 14:30:13 +0800

    添加友链页面和导航入口

--- src/config.ts
+++ src/config.ts
@@ -45,6 +45,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
+			name: "友链",
+			url: "/friends/",
+			external: false,
+		},
+		{
 			name: "wenject",
 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
