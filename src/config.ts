e2e6c840c3fd397694a7c28b323173d350c8518e
wenject <2256475223@qq.com>
2026-02-12 14:24:20 +0800

    更新博客内容

--- src/config.ts
+++ src/config.ts
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
+	name: "wenject",
 	bio: "一个小菜鸡罢了",
 	links: [
 		{
